cef98d674d15420275725d98dd4bd19a06a33ecf
Ted <t.aldentempleman@gmail.com>
2023-02-13 18:24:22 +0000

    Update main.py
    Refactored main.py to use a Catan class to encapsulate the game.

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import pygame
 from board import Board
 
-pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+SCREEN_FPS = 30
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 800
 
-b = Board(50, 45)
 
-running = True
-while running:
+class Catan:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    def __init__(self):
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.board = Board(50, 45)
+        self.running = True
 
-    screen.fill((255, 255, 255))
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
-    b.draw(screen)
+        self.clock.tick(30)
 
-    pygame.display.flip()
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        self.board.draw(self.screen)
+        pygame.display.flip()
 
-pygame.quit()
+
+def main():
+    pygame.init()
+    c = Catan()
+
+    while c.running:
+        c.update()
+        c.draw()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
