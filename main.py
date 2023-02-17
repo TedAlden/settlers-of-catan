69e756f5bdbecd8ef9704dab68213d68f60fb19d
Ted <t.aldentempleman@gmail.com>
2023-02-17 16:08:39 +0000

    Refactored main.py

--- main.py
+++ main.py
@@ -11,35 +11,37 @@ SCREEN_TITLE = "The Settlers of Catan"
 class Catan:
 
     def __init__(self):
+        pygame.init()
         pygame.display.set_caption(SCREEN_TITLE)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.board = Board(50, 45)
-        self.running = True
+        self._running = True
 
-    def update(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
 
+    def on_update(self):
         self.clock.tick(30)
 
-    def draw(self):
+    def on_render(self):
         self.screen.fill((255, 255, 255))
         self.board.draw(self.screen)
         pygame.display.flip()
 
+    def on_execute(self):
+        while(self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_update()
+            self.on_render()
+        self.on_cleanup()
 
-def main():
-    pygame.init()
-    c = Catan()
-
-    while c.running:
-        c.update()
-        c.draw()
-
-    pygame.quit()
+    def on_cleanup(self):
+        pygame.quit()
 
 
 if __name__ == "__main__":
-    main()
+    c = Catan()
+    c.on_execute()
