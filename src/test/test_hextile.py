4dc44abc1100258eb5496121944b44cb9e075950
ls2009 <124938441+ls2009@users.noreply.github.com>
2023-04-27 18:30:52 +0100

    Update test_hextile.py

--- src/test/test_hextile.py
+++ src/test/test_hextile.py
@@ -1,1 +1,22 @@
+import unittest
+from hextile import HexTile  
+from unittest.mock import patch
+
+
+class TestHexTile(unittest.TestCase):
+
+    def setUp(self):
+        with patch("hextile.pygame.sprite.Sprite.__init__"):
+            with patch("hextile.pygame.Surface"):
+                self.axial_coord = (1, 2)
+                self.hextile = HexTile(self.axial_coord)
+
+    def test_set_and_get_pos(self):
+        center_x, center_y = 100, 200
+        self.hextile.set_pos(center_x, center_y)
+        self.assertEqual(self.hextile.get_pos(), (center_x, center_y), "Failed to set and get position correctly")
+
+
+if __name__ == '__main__':
+    unittest.main()
 
