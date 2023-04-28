7f11d2bfa7b8819e317d5df3cfaf38b2d472af3b
NawafQ55 <125082129+NawafQ55@users.noreply.github.com>
2023-04-28 15:50:03 +0300

    fixed hextile  test

--- src/test/test_hextile.py
+++ src/test/test_hextile.py
@@ -1,22 +1,20 @@
 import unittest
-from hextile import HexTile  
-from unittest.mock import patch
-
+from catan.models.hextile import HexTile
 
 class TestHexTile(unittest.TestCase):
 
     def setUp(self):
-        with patch("hextile.pygame.sprite.Sprite.__init__"):
-            with patch("hextile.pygame.Surface"):
-                self.axial_coord = (1, 2)
-                self.hextile = HexTile(self.axial_coord)
+        self.hex_tile = HexTile((0,0))
 
-    def test_set_and_get_pos(self):
-        center_x, center_y = 100, 200
-        self.hextile.set_pos(center_x, center_y)
-        self.assertEqual(self.hextile.get_pos(), (center_x, center_y), "Failed to set and get position correctly")
+    def test_init(self):
+        self.assertEqual(self.hex_tile.axial_coord, (0,0))
+        self.assertEqual(self.hex_tile.radius, 55)
+        self.assertIsNone(self.hex_tile.type)
+        self.assertEqual(self.hex_tile.number, -1)
 
+    def test_set_pos(self):
+        self.hex_tile.set_pos(100, 200)
+        self.assertEqual(self.hex_tile.get_pos(), (100, 200))
 
 if __name__ == '__main__':
     unittest.main()
-
